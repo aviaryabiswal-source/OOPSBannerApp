6f28d891aa704555d3597d593c6ca004f93485be
aviaryabiswal-source <aviaryabiswal@gmail.com>
2026-03-11 13:52:52 +0530

    done

---
+++ classes.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class A{
+    public:
+    void display();
+
+    void B::display() {
+        b=y;
+};
+int main() {
+    A obj;
+    obj.display();
+    return 0;
+}
